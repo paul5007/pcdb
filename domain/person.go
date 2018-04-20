216deafddaa7ba36004b505747aebd56154992a9
Paul Ascenzi <pfa5007@gmail.com>
2018-04-20 00:25:32 -0400

    removed generic validation

--- domain/person.go
+++ domain/person.go
@@ -1,30 +1,8 @@
 package domain
 
-import (
-	"fmt"
-)
-
 // Person holds information on a person
 type Person struct {
 	ID   string
 	Name string
 	Age  int
-}
-
-// Valid checks to make sure Person object is valid
-func (p *Person) Valid() (bool, error) {
-	var err error
-	if p.ID == "" {
-		err = fmt.Errorf("ID is nil ")
-	}
-	if p.Name == "" {
-		err = fmt.Errorf("%v | Name is nil", err)
-	}
-	if p.Age <= 0 {
-		err = fmt.Errorf("%v | Age: %v <= 0", err, p.Age)
-	}
-	if err != nil {
-		return false, err
-	}
-	return true, nil
 }
