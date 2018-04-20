e88c8e13c55cebfba6b3e8b76bb944a078ffa428
Paul Ascenzi <pfa5007@gmail.com>
2018-04-20 00:20:36 -0400

    added a service along with domain object to show how to use PCDB

---
+++ service/person_service.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/paul5007/pcdb/database"
+	"github.com/paul5007/pcdb/domain"
+)
+
+const (
+	regionName = "Person"
+)
+
+// PersonService allows manipulation of Persons within the database and service layer
+type PersonService struct{}
+
+// AddPerson adds a new person to the database
+func (ps *PersonService) AddPerson(db database.PaulCacheDatabase, p domain.Person) error {
+	r, err := db.GetRegion(regionName)
+	if err != nil {
+		return fmt.Errorf("Failed to find region: %v | %v", regionName, err)
+	}
+	valid, err := p.Valid()
+	if !valid {
+		return fmt.Errorf("Failed to validated Person: %v | %v", p, err)
+	}
+	err = r.Add(p.ID, p)
+	if err != nil {
+		return fmt.Errorf("Failed to add Person: %v to region: %v | %v", p, regionName, err)
+	}
+	return nil
+}
+
+// RemovePerson removes a person from the database
+func (ps *PersonService) RemovePerson(db database.PaulCacheDatabase, p domain.Person) error {
+	r, err := db.GetRegion(regionName)
+	if err != nil {
+		return fmt.Errorf("Failed to find region: %v | %v", regionName, err)
+	}
+	err = r.Remove(p.ID)
+	if err != nil {
+		return fmt.Errorf("Failed to remove Person: %v from region: %v | %v", p, regionName, err)
+	}
+	return nil
+}
