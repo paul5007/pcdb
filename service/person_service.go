3d2906a5594a6c329efb2a86547f74146071c7f7
Paul Ascenzi <pfa5007@gmail.com>
2018-04-21 12:34:34 -0400

    added get all to personService

--- service/person_service.go
+++ service/person_service.go
@@ -2,6 +2,9 @@ package service
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
 
 	"github.com/paul5007/pcdb/database"
 	"github.com/paul5007/pcdb/domain"
@@ -9,6 +12,10 @@ import (
 
 const (
 	regionName = "Person"
+)
+
+var (
+	idCounter int
 )
 
 // PersonService allows manipulation of Persons within the database and service layer
@@ -21,7 +28,9 @@ func (ps *PersonService) AddPerson(db database.PaulCacheDatabase, p domain.Perso
 		return fmt.Errorf("Failed to find region: %v | %v", regionName, err)
 	}
 
-	err = r.Add(p.ID, p)
+	id := generateID()
+	p.ID = id
+	err = r.Add(id, p)
 	if err != nil {
 		return fmt.Errorf("Failed to add Person: %v to region: %v | %v", p, regionName, err)
 	}
@@ -29,14 +38,41 @@ func (ps *PersonService) AddPerson(db database.PaulCacheDatabase, p domain.Perso
 }
 
 // RemovePerson removes a person from the database
-func (ps *PersonService) RemovePerson(db database.PaulCacheDatabase, p domain.Person) error {
+func (ps *PersonService) RemovePerson(db database.PaulCacheDatabase, id string) error {
 	r, err := db.GetRegion(regionName)
 	if err != nil {
 		return fmt.Errorf("Failed to find region: %v | %v", regionName, err)
 	}
-	err = r.Remove(p.ID)
+	err = r.Remove(id)
 	if err != nil {
-		return fmt.Errorf("Failed to remove Person: %v from region: %v | %v", p, regionName, err)
+		return fmt.Errorf("Failed to remove Person with ID: %v from region: %v | %v", id, regionName, err)
 	}
 	return nil
+}
+
+// ListPersons will return all persons from database
+func (ps *PersonService) ListPersons(db database.PaulCacheDatabase) ([]domain.Person, error) {
+	r, err := db.GetRegion(regionName)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to find region: %v | %v", regionName, err)
+	}
+	list := r.GetAll()
+	var personList []domain.Person
+	for _, p := range list {
+		person, ok := p.(domain.Person)
+		if ok {
+			personList = append(personList, person)
+		}
+	}
+	return personList, nil
+}
+
+func generateID() string {
+	time := time.Now()
+	rand.Seed(time.Unix())
+
+	rnd := rand.Intn(1000000)
+
+	id := "d" + strconv.FormatInt(int64(rnd), 10)
+	return id
 }
